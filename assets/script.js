18b5c07f1c3a1bcfb309deb121054f3f2644b18b
Vinicius S <viniciuseduardobsignorelli@gmail.com>
2023-12-21 21:02:21 -0300

    inicio

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+function scrollToElement(elementSelector, instance = 0) {
+    // Select all elements that match the given selector
+    const elements = document.querySelectorAll(elementSelector);
+    // Check if there are elements matching the selector and if the requested instance exists
+    if (elements.length > instance) {
+        // Scroll to the specified instance of the element
+        elements[instance].scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+link1.addEventListener('click', () => {
+    scrollToElement('.header');
+});
+
+link2.addEventListener('click', () => {
+    // Scroll to the second element with "header" class
+    scrollToElement('.header', 1);
+});
+
+link3.addEventListener('click', () => {
+    scrollToElement('.column');
+});
+
+document.getElementById('emailButton').addEventListener('click', function() {
+    window.location.href = 'mailto:viniciuseduardobsignorelli@gmail.com';
+});
+var pos= document.documentElement; 
+pos.addEventListener('mousemove', e => {
+    pos.style.setProperty('--x',e.clientX + 'px')
+    pos.style.setProperty('--y',e.clientY + 'px')
+})
